59fa569c986518cad371a525e9dd4a570041413f
Guerre-Pablo-Agustin <guerre.pablo.agustin@gmail.com>
2024-09-14 19:13:49 -0300

    about y footer

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,8 +1,13 @@
 const About = () => {
   return (
-    <section className="mt-4 flex w-full flex-col gap-3 px-8  mb-5">
-      <h2 className="text-3xl text-center">Sobre mi</h2>
-      <p className="text-center">
+    <section className="mt-4 ">
+      <h2 className="text-3xl text-center mb-3">Sobre mi</h2>
+      <div className="flex w-full flex-col md:flex-row gap-3 px-8  mb-5">
+      <div className="px-8">
+        <img  src='../../../public/images/about.png' alt="" />
+       </div>
+      <div className="px-8">
+      <p className="text-center mt-3">
         Me llamo Pablo Agustin Guerreño. Soy Técnico Superior en Programación y
         Análisis de Sistemas. Me interesa seguir creciendo en el desarrollo web,
         estoy buscando oportunidades como Desarrollador full stack o front end.
@@ -10,15 +15,17 @@ const About = () => {
         experiencia del usuario,trabajar en equipo para aportar mis
         conocimientos y aprender de mis compañeros. </p>
         <br />
-        <p className="text-center">
+        <p className="text-center mt-3">
           Tengo conocimientos en Next.js, React, Tailwind Css, Redux/toolkit,
           PostgreSQL, Express, Node.js y me gusta aprender nuevas tecnologías.
         </p>
 
-        <p className="text-center">
+        <p className="text-center mt-2">
         Este es mi portfolio: <a target="_blank" href="https://guerrepablo-portfolio.netlify.app">https://guerrepablo-portfolio.netlify.app</a> 
         </p>
-       
+      </div>
+  
+      </div>
     
     </section>
   );
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+   <footer className="text-center text-white bg-black p-4">
+    <p>© 2024</p>
+   </footer>
+  )
+}
+
+export default Footer
